9097a146e3a8697697befa7afb85c240cae58739
Pablosanserr <88164280+Pablosanserr@users.noreply.github.com>
2024-05-27 18:11:56 +0200

    Initial 3G Auth code

---
+++ sim.py
@@ -0,0 +1,24 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+import os
+
+class UIM:
+    def __init__(self):
+
+class Movil:
+    def __init__(self):
+
+class Antena:
+
+    def __init__(self):
+
+class Operador: 
+
+    def __init__(self):
+
+# ------------------------ MAIN ------------------------
+
+uim = UIM()
+movil = Movil()
+antena = Antena()
+operador = Operador()
